01b008f6650add53ee6f9147fbea4343f962049e
Maël Thomas <thomas-m02@saint.louis29.net>
2026-01-29 15:06:23 +0100

    Création de la fonction _ajouter_tuile

--- src/sl29/games/_2048.py
+++ src/sl29/games/_2048.py
@@ -1,6 +1,7 @@
 """Module providing the logic of the 2048 game"""
 
 import random
+import copy
 from typing import List, Tuple
 
 TAILLE:int = 4
@@ -79,7 +80,25 @@ def _ajouter_tuile(plateau: List[List[int]]) -> List[List[int]]:
     :return: Une nouvelle grille avec une tuile ajoutée.
     :rtype: List[List[int]]
     """
-    raise NotImplementedError("Fonction _ajouter_tuile non implémentée.")
+
+    nouveau_plateau = copy.deepcopy(plateau)
+
+    # Récupérer les cases vides
+    cases_vides = _get_cases_vides(nouveau_plateau)
+
+    # S'il n'y a plus de place, on retourne le plateau tel quel
+    if not cases_vides:
+        return nouveau_plateau
+
+    # Choisir une case vide au hasard
+    ligne, colonne = random.choice(cases_vides)
+
+    # Ajouter une tuile (2)
+    nouveau_plateau[ligne][colonne] = 2
+
+    return nouveau_plateau
+
+
 
 def _supprimer_zeros(ligne: List[int]) -> List[int]:
     """
